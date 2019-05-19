292b38f0c3c4210b22e42fa676810b5aeee326bf
Samuel Ainsworth <skainsworth@gmail.com>
2019-05-19 11:56:14 -0700

    pickle q learning results script

--- frozenlake/run_q_learning.py
+++ frozenlake/run_q_learning.py
@@ -2,7 +2,13 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+import functools
+from multiprocessing import Pool
+from pathlib import Path
+import pickle
+
 import numpy as np
+import tqdm
 
 import frozenlake
 import viz
@@ -28,9 +34,13 @@ def epsilon_greedy_annealed(epsilon: float):
 
   return h
 
-def run_q_learning(env: frozenlake.FrozenLakeEnv,
-                   gamma: float,
-                   policy_evaluation_frequency: int = 10):
+def run_q_learning(
+    env: frozenlake.FrozenLakeEnv,
+    gamma: float,
+    num_episodes: int,
+    policy_evaluation_frequency: int = 10,
+    verbose: bool = True,
+):
   # Initializing to random values is necessary to break ties, preventing the
   # agent from always picking the same action and never getting anywhere.
   Q = np.random.rand(env.lake.num_states, frozenlake.NUM_ACTIONS)
@@ -46,7 +56,7 @@ def run_q_learning(env: frozenlake.FrozenLakeEnv,
   states_seen = 0
   states_seen_log = []
   policy_rewards_log = []
-  for episode_num in range(5000):
+  for episode_num in range(num_episodes):
     Q, episode, _ = frozenlake.q_learning_episode(
         env,
         gamma,
@@ -62,7 +72,9 @@ def run_q_learning(env: frozenlake.FrozenLakeEnv,
       V, _ = frozenlake.iterative_policy_evaluation(
           env, gamma, policy, tolerance=1e-6, init_V=V)
       policy_reward = np.dot(V, env.initial_state_distribution)
-      print(f"Episode {episode_num}, policy reward: {policy_reward}")
+
+      if verbose:
+        print(f"Episode {episode_num}, policy reward: {policy_reward}")
 
       states_seen_log.append(states_seen)
       policy_rewards_log.append(policy_reward)
@@ -76,7 +88,26 @@ def run_q_learning(env: frozenlake.FrozenLakeEnv,
 
   return states_seen_log, policy_rewards_log
 
-if __name__ == "__main__":
+def q_learning_job(random_seed: int, env, gamma: float,
+                   policy_evaluation_frequency: int, folder: Path):
+  np.random.seed(random_seed)
+
+  states_seen, policy_rewards = run_q_learning(
+      env,
+      gamma,
+      policy_evaluation_frequency=policy_evaluation_frequency,
+      num_episodes=10,
+      verbose=False)
+
+  with (folder / f"seed={random_seed}.pkl").open(mode="wb") as f:
+    pickle.dump({
+        "states_seen": states_seen,
+        "policy_rewards": policy_rewards
+    }, f)
+
+  # print(f"Finished job for random seed {random_seed}.")
+
+def main():
   np.random.seed(0)
 
   def build_env(lake: frozenlake.Lake):
@@ -87,7 +118,19 @@ if __name__ == "__main__":
   lake_map = frozenlake.MAP_8x8
   policy_evaluation_frequency = 10
   gamma = 0.99
+  num_random_seeds = 100
 
+  results_dir = Path("qlearning_pkls")
+  estop_results_dir = results_dir / "estop"
+  full_results_dir = results_dir / "full"
+  results_dir.mkdir()
+  estop_results_dir.mkdir()
+  full_results_dir.mkdir()
+
+  pool = Pool()
+
+  # Build the full environment and run value iteration to calculate the optimal
+  # policy.
   lake = frozenlake.Lake(lake_map)
   env = build_env(lake)
   state_action_values, _ = frozenlake.value_iteration(
@@ -95,56 +138,101 @@ if __name__ == "__main__":
   state_values = np.max(state_action_values, axis=-1)
   optimal_policy_reward = np.dot(state_values, env.initial_state_distribution)
 
-  # E-stop environment.
+  # Estimate hitting probabilities.
   optimal_policy = frozenlake.deterministic_policy(
       env, np.argmax(state_action_values, axis=-1))
   estimated_hp = frozenlake.estimate_hitting_probabilities(
       env, optimal_policy, num_rollouts=1000)
   estimated_hp2d = lake.reshape(estimated_hp)
 
+  # Build e-stop environment.
   estop_map = np.copy(lake_map)
   percentile = 50
   threshold = np.percentile(estimated_hp, percentile)
   estop_map[estimated_hp2d <= threshold] = "E"
 
   estop_lake = frozenlake.Lake(estop_map)
-
-  plt.figure()
-  viz.plot_heatmap(estop_lake, np.zeros(estop_lake.num_states))
-  plt.title("E-stop map")
-  plt.show()
-
   estop_env = build_env(estop_lake)
-  estop_states_seen, estop_policy_rewards = run_q_learning(
-      estop_env,
-      gamma,
-      policy_evaluation_frequency=policy_evaluation_frequency)
 
-  # Full environment.
-  full_map_states_seen, full_map_policy_rewards = run_q_learning(
-      env, gamma, policy_evaluation_frequency=policy_evaluation_frequency)
+  # pickle dump the environemnt setup/metadata...
+  pickle.dump({
+      "lake_map": lake_map,
+      "policy_evaluation_frequency": policy_evaluation_frequency,
+      "gamma": gamma,
+      "num_random_seeds": num_random_seeds,
+      "lake": lake,
+      "env": env,
+      "state_action_values": state_action_values,
+      "state_values": state_values,
+      "optimal_policy_reward": optimal_policy_reward,
+      "optimal_policy": optimal_policy,
+      "estimated_hp": estimated_hp,
+      "estimated_hp2d": estimated_hp2d,
+      "estop_map": estop_map,
+      "percentile": percentile,
+      "threshold": threshold,
+      "estop_lake": estop_lake,
+      "estop_env": estop_env,
+  }, (results_dir / "metadata.pkl").open(mode="wb"))
+
+  # plt.figure()
+  # viz.plot_heatmap(estop_lake, np.zeros(estop_lake.num_states))
+  # plt.title("E-stop map")
+  # plt.show()
+
+  # Run Q-learning on the full environment.
+  for _ in tqdm.tqdm(
+      pool.imap_unordered(
+          functools.partial(
+              q_learning_job,
+              env=env,
+              gamma=gamma,
+              policy_evaluation_frequency=policy_evaluation_frequency,
+              folder=estop_results_dir,
+          ), range(num_random_seeds)),
+      desc="full",
+      total=num_random_seeds):
+    pass
+
+  # Run Q-learning on the e-stop environment.
+  for _ in tqdm.tqdm(
+      pool.imap_unordered(
+          functools.partial(
+              q_learning_job,
+              env=estop_env,
+              gamma=gamma,
+              policy_evaluation_frequency=policy_evaluation_frequency,
+              folder=estop_results_dir,
+          ), range(num_random_seeds)),
+      desc="estop",
+      total=num_random_seeds):
+    pass
 
   ### Plotting
-  plt.figure()
-  plt.plot(
-      policy_evaluation_frequency * np.arange(len(full_map_policy_rewards)),
-      full_map_policy_rewards)
-  plt.plot(policy_evaluation_frequency * np.arange(len(estop_policy_rewards)),
-           estop_policy_rewards)
-  plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
-  plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
-  plt.xlabel("Episode")
-  plt.ylabel("Policy reward")
-  plt.savefig("figs/q_learning_per_episode.pdf")
+  # plt.figure()
+  # plt.plot(
+  #     policy_evaluation_frequency * np.arange(len(full_map_policy_rewards)),
+  #     full_map_policy_rewards)
+  # plt.plot(policy_evaluation_frequency * np.arange(len(estop_policy_rewards)),
+  #          estop_policy_rewards)
+  # plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
+  # plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
+  # plt.xlabel("Episode")
+  # plt.ylabel("Policy reward")
+  # plt.savefig("figs/q_learning_per_episode.pdf")
 
-  plt.figure()
-  plt.plot(full_map_states_seen, full_map_policy_rewards)
-  plt.plot(estop_states_seen, estop_policy_rewards)
-  plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
-  plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
-  plt.xlabel("Number of states seen")
-  plt.ylabel("Policy reward")
-  # plt.title("Q-learning on the complete environment")
-  plt.savefig("figs/q_learning_per_states_seen.pdf")
+  # plt.figure()
+  # plt.plot(full_map_states_seen, full_map_policy_rewards)
+  # plt.plot(estop_states_seen, estop_policy_rewards)
+  # plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
+  # plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
+  # plt.xlabel("Number of states seen")
+  # plt.ylabel("Policy reward")
+  # # plt.title("Q-learning on the complete environment")
+  # plt.savefig("figs/q_learning_per_states_seen.pdf")
 
-  plt.show()
+  # plt.show()
+
+if __name__ == "__main__":
+  progress_bar = None
+  main()
