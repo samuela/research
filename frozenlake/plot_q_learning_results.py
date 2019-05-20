4d872df51192321886360b54fcd6a4b00b6698a1
Samuel Ainsworth <skainsworth@gmail.com>
2019-05-19 19:38:05 -0700

    switch to median instead of mean

--- frozenlake/plot_q_learning_results.py
+++ frozenlake/plot_q_learning_results.py
@@ -10,7 +10,8 @@ import numpy as np
 def plot_errorfill(x, ys, color):
   mean = np.mean(ys, axis=0)
   std = np.std(ys, axis=0)
-  plt.plot(x, mean, color=color, alpha=1.0)
+  #   plt.plot(x, mean, color=color, alpha=1.0)
+  plt.plot(x, np.median(ys, axis=0), color=color, alpha=1.0)
   plt.fill_between(x,
                    np.maximum(mean - std, ys.min(axis=0)),
                    np.minimum(mean + std, ys.max(axis=0)),
