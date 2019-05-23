2db9c3772331bb02a8df1e578549adc7b04ea23f
Samuel Ainsworth <skainsworth@gmail.com>
2019-05-22 18:04:22 -0700

    add actor critic plotting

--- frozenlake/plot_actor_critic_results.py
+++ frozenlake/plot_actor_critic_results.py
@@ -10,7 +10,7 @@ import numpy as np
 import viz
 
 if __name__ == "__main__":
-  results_dir = Path("results/qlearning_pkls_444a731")
+  results_dir = Path("results/actor_critic_pkls_e9c2c7c")
 
   metadata = pickle.load((results_dir / "metadata.pkl").open(mode="rb"))
   num_random_seeds = metadata["num_random_seeds"]
@@ -35,26 +35,24 @@ if __name__ == "__main__":
   full_states_seen = np.array([run["states_seen"] for run in full_results])
 
   ### Plot per episode.
-  num_episodes_to_plot = 2500
-  num_points_to_plot = int(num_episodes_to_plot / policy_evaluation_frequency)
-
   plt.rcParams.update({"font.size": 16})
 
   plt.figure()
-  x = policy_evaluation_frequency * np.arange(num_points_to_plot)
-  viz.plot_errorfill(x, full_policy_rewards[:, :num_points_to_plot], "slategrey")
-  viz.plot_errorfill(x, estop_policy_rewards[:, :num_points_to_plot], "crimson")
+  x = policy_evaluation_frequency * np.arange(full_policy_rewards.shape[1])
+  viz.plot_errorfill(x, full_policy_rewards, "slategrey")
+  viz.plot_errorfill(x, estop_policy_rewards, "crimson")
   plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
-  plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
+  plt.legend(
+      ["Full env. Actor-Critic", "E-stop Actor-Critic", "Optimal policy"])
   plt.xlabel("Episode")
   plt.ylabel("Policy reward")
   plt.tight_layout()
-  plt.savefig("figs/q_learning_per_episode.pdf")
+  plt.savefig("figs/actor_critic_per_episode.pdf")
 
   ### Plot per states seen.
   # Plotting every single state is really overkill on this scale and makes
   # vector graphics huge and sluggish.
-  num_states_seen_to_plot = 550000
+  num_states_seen_to_plot = 5e6
   freq = 1000
   x = freq * np.arange(int(num_states_seen_to_plot / freq))
   estop_policy_rewards_interp = np.array([
@@ -76,10 +74,10 @@ if __name__ == "__main__":
   viz.plot_errorfill(x / 1000, full_policy_rewards_interp, "slategrey")
   viz.plot_errorfill(x / 1000, estop_policy_rewards_interp, "crimson")
   plt.axhline(optimal_policy_reward, color="grey", linestyle="--")
-  plt.legend(["Full env. Q-learning", "E-stop Q-learning", "Optimal policy"])
+  plt.legend(
+      ["Full env. Actor-Critic", "E-stop Actor-Critic", "Optimal policy"],
+      loc="lower right")
   plt.xlabel("Number of states seen (thousands)")
   plt.ylabel("Policy reward")
   plt.tight_layout()
-  plt.savefig("figs/q_learning_per_states_seen.pdf")
-
-  plt.show()
+  plt.savefig("figs/actor_critic_per_states_seen.pdf")
--- frozenlake/viz.py
+++ frozenlake/viz.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 import frozenlake
 
@@ -17,3 +18,14 @@ def plot_heatmap(lake: frozenlake.Lake, heat1d):
         })
 
   return im
+
+def plot_errorfill(x, ys, color):
+  mean = np.mean(ys, axis=0)
+  std = np.std(ys, axis=0)
+  #   plt.plot(x, mean, color=color, alpha=1.0)
+  plt.plot(x, np.median(ys, axis=0), color=color, alpha=1.0)
+  plt.fill_between(x,
+                   np.maximum(mean - std, ys.min(axis=0)),
+                   np.minimum(mean + std, ys.max(axis=0)),
+                   color=color,
+                   alpha=0.25)
