12f118415c1e77ea44d3b9209813b5e6e9668a1b
Samuel Ainsworth <skainsworth@gmail.com>
2019-07-29 20:06:09 -0700

    viz for support sets and rollouts

--- research/estop/pendulum/viz_policy_rollouts.py
+++ research/estop/pendulum/viz_policy_rollouts.py
@@ -1,6 +1,7 @@
 import pickle
 
 from jax import random
+import jax.numpy as jp
 
 from research.estop import ddpg
 from research.estop.pendulum import config
@@ -8,18 +9,28 @@ from research.estop.pendulum.env import viz_pendulum_rollout
 from research.estop.pendulum.run_ddpg import actor
 from research.statistax import Deterministic
 
-experiment_folder = "5_15ba7ed_ddpg_pendulum"
-seed = 0
-episode = -1
+experiment_folder = "ddpg_pendulum"
+experiment_metadata = pickle.load(
+    open(f"results/{experiment_folder}/metadata.pkl", "rb"))
+num_random_seeds = experiment_metadata["num_random_seeds"]
 
-data = pickle.load(
-    open(f"results/{experiment_folder}/full/seed={seed}.pkl", "rb"))
-actor_params, _ = data["params_per_episode"][episode]
+data = [
+    pickle.load(open(f"results/{experiment_folder}/full/seed={seed}.pkl",
+                     "rb")) for seed in range(num_random_seeds)
+]
+final_policy_values = jp.array(
+    [x["policy_value_per_episode"][-1] for x in data])
+best_seed = int(jp.argmax(final_policy_values))
+
+print(f"Best seed: {best_seed}")
+print(f"Policy cumulative reward: {final_policy_values[best_seed]}")
+
+actor_params, _ = data[best_seed]["final_params"]
 
 rng = random.PRNGKey(0)
 while True:
   rollout_rng, rng = random.split(rng)
-  states, actions = ddpg.rollout(
+  states, actions, _ = ddpg.rollout(
       rollout_rng,
       config.env,
       lambda s: Deterministic(actor(actor_params, s)),
