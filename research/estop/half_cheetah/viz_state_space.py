b03086e7a99317c009b4c950b66c9b8145696ea5
Samuel Ainsworth <skainsworth@gmail.com>
2019-09-28 23:42:57 -0700

    add viz_state_space

---
+++ research/estop/half_cheetah/viz_state_space.py
@@ -0,0 +1,24 @@
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+import tqdm
+import numpy as np
+
+from research.estop.half_cheetah import config
+
+env = config._gym_env
+
+delta = 2
+env.reset()
+mjstate = env.sim.get_state()
+qpos = mjstate.qpos
+
+for i in tqdm.trange(len(qpos)):
+  video = VideoRecorder(env, path=f"qpos_{i}_sweep.mp4")
+
+  a = qpos - delta * np.eye(len(qpos))[i]
+  b = qpos + delta * np.eye(len(qpos))[i]
+  for t in np.linspace(0, 1):
+    env.sim.set_state(mjstate._replace(qpos=(1 - t) * a + t * b))
+    env.sim.step()
+    video.capture_frame()
+
+  video.close()
