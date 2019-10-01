77955d3a2d7e32b0182aa037db010e47ef6a8d15
Samuel Ainsworth <skainsworth@gmail.com>
2019-10-01 00:37:05 -0700

    remove env_spec and switch to env_name and reward_adjustment

--- research/estop/gym/half_cheetah/__init__.py
+++ research/estop/gym/half_cheetah/__init__.py
@@ -1,3 +1,2 @@
-from research.estop.gym.gym_wrappers import build_env_spec
-
-env_spec = build_env_spec("HalfCheetah-v3", reward_adjustment=1.0)
+env_name = "HalfCheetah-v3"
+reward_adjustment = 1.0
