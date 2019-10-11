58c678cc8c017bb6417f7ea32d7109e5b63094ef
Samuel Ainsworth <skainsworth@gmail.com>
2019-10-11 14:03:57 -0700

    fix pylint

--- research/estop/gym/half_cheetah/ddpg/debug_run_estop.py
+++ research/estop/gym/half_cheetah/ddpg/debug_run_estop.py
@@ -25,7 +25,7 @@ policy_video_frequency = 1000
 env_spec = build_env_spec(env_name, reward_adjustment)
 train_config = make_default_ddpg_train_config(env_spec)
 
-def run_expert_rollouts(rng):
+def run_expert_rollouts(rng) -> np.ndarray:
   print("Loading vanilla DDPG results...")
   experiment_metadata = pickle.load(
       (input_results_dir / "metadata.pkl").open("rb"))
@@ -36,9 +36,8 @@ def run_expert_rollouts(rng):
   ]
   final_policy_values = np.array([x["policy_evaluations"][-1] for x in data])
   best_seed = int(np.argmax(final_policy_values))
-  print(
-      f"... best seed is {best_seed} with cumulative reward: {data[best_seed]['policy_evaluations'][-1]}"
-  )
+  print(f"... best seed is {best_seed}"
+        f"with cumulative reward: {data[best_seed]['policy_evaluations'][-1]}")
 
   print("Rolling out trajectories from best policy...")
   actor_params, _ = data[best_seed]["final_params"]
@@ -68,7 +67,7 @@ def get_estop_bounds(expert_rollouts):
   std = np.std(expert_rollouts, axis=(0, 1))
   return state_min - std, state_max + std
 
-if __name__ == "__main__":
+def main():
   rng = random.PRNGKey(0)
 
   expert_rollouts = run_expert_rollouts(rng)
@@ -93,3 +92,6 @@ if __name__ == "__main__":
             seed=0,
             state_min=state_min,
             state_max=state_max)
+
+if __name__ == "__main__":
+  main()
