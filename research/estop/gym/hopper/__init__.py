915a2e5ef9ecaf4162aa7eb0292300764a4b6e54
Samuel Ainsworth <skainsworth@gmail.com>
2019-10-16 15:51:46 -0700

    add hopper run_batch and debug_run_gymstop

---
+++ research/estop/gym/hopper/__init__.py
@@ -0,0 +1,2 @@
+env_name = "Hopper-v3"
+reward_adjustment = 0.1
---
+++ research/estop/gym/hopper/ddpg/debug_run_gymstop.py
@@ -0,0 +1,8 @@
+from research.estop.gym.ddpg_training import debug_run, make_default_ddpg_train_config
+from research.estop.gym.hopper import env_name, reward_adjustment
+from research.estop.gym.gym_wrappers import build_env_spec
+
+env_spec = build_env_spec(env_name, reward_adjustment)
+debug_run(env_spec,
+          make_default_ddpg_train_config(env_spec),
+          respect_gym_done=True)
