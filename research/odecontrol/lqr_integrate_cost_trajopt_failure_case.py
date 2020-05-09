eed4c5f0163cb4ca6a71c944ba09b6a86330dba4
Samuel Ainsworth <skainsworth@gmail.com>
2020-05-08 18:51:46 -0700

    split the cost into position cost and control cost and add some more logging

--- research/odecontrol/lqr_integrate_cost_trajopt_failure_case.py
+++ research/odecontrol/lqr_integrate_cost_trajopt_failure_case.py
@@ -39,18 +39,18 @@ def random_env(rng):
   N = jp.zeros((2, 2))
   return A, B, Q, R, N
 
-def policy_integrate_cost(dynamics_fn, cost_fn, gamma, policy):
+def policy_integrate_cost(dynamics_fn, position_cost_fn, control_cost_fn, gamma, policy):
   # Specialize to the environment.
 
   def ofunc(y, t, policy_params):
-    _, x = y
+    _, _, x = y
     u = policy(policy_params, x)
-    return ((gamma**t) * cost_fn(x, u), dynamics_fn(x, u))
+    return ((gamma**t) * position_cost_fn(x), (gamma**t) * control_cost_fn(u), dynamics_fn(x, u))
 
   def eval_from_x0(policy_params, x0, total_time):
     # Zero is necessary for some reason...
     ts = jp.array([0.0, total_time])
-    y0 = (jp.zeros(()), x0)
+    y0 = (jp.zeros(()), jp.zeros(()), x0)
     odeint_kwargs = {"mxstep": 1e6}
     y_fwd = ode.odeint(ofunc, y0, ts, policy_params, **odeint_kwargs)
     yT = tree_map(itemgetter(1), y_fwd)
@@ -79,22 +79,41 @@ def main():
   # u = - Kx
   # cost = xQx + uRu + 2xNu
   A, B, Q, R, N = fixed_env(x_dim)
+  print("System dynamics:")
+  print(f"  A = {A}")
+  print(f"  B = {B}")
+  print(f"  Q = {Q}")
+  print(f"  R = {R}")
+  print(f"  N = {N}")
+  print()
+
   dynamics_fn = lambda x, u: A @ x + B @ u
-  cost_fn = lambda x, u: x.T @ Q @ x + u.T @ R @ u + 2 * x.T @ N @ u
-  # position_cost_fn = lambda x, u: x.T @ Q @ x
-  # control_cost_fn = lambda x, u: u.T @ R @ u
+  # cost_fn = lambda x, u: x.T @ Q @ x + u.T @ R @ u + 2 * x.T @ N @ u
+  position_cost_fn = lambda x: x.T @ Q @ x
+  control_cost_fn = lambda u: u.T @ R @ u
 
   ### Solve the Riccatti equation to get the infinite-horizon optimal solution.
   K, _, _ = control.lqr(A, B, Q, R, N)
   K = jp.array(K)
 
   t0 = time.time()
-  _, (opt_cost_fwd, opt_xT_fwd), (opt_cost_bwd, opt_x0_bwd) = policy_integrate_cost(
-      dynamics_fn, cost_fn, gamma, lambda _, x: -K @ x)(None, x0, total_time)
-  print(f"opt_cost_fwd = {opt_cost_fwd}, opt_cost_bwd = {opt_cost_bwd} in {time.time() - t0}s")
-  print(opt_xT_fwd)
-  print(opt_x0_bwd)
-  print(f"l2 error: {jp.sqrt(jp.sum((x0 - opt_x0_bwd)**2))}")
+  _, (opt_x_cost_fwd, opt_u_cost_fwd,
+      opt_xT_fwd), (opt_x_cost_bwd, opt_u_cost_bwd,
+                    opt_x0_bwd) = policy_integrate_cost(dynamics_fn, position_cost_fn,
+                                                        control_cost_fn, gamma,
+                                                        lambda _, x: -K @ x)(None, x0, total_time)
+  opt_cost_fwd = opt_x_cost_fwd + opt_u_cost_fwd
+  print("LQR solution:")
+  print(f"  K                     = {K}")
+  print(f"  opt_x_cost_fwd        = {opt_x_cost_fwd}")
+  print(f"  opt_u_cost_fwd        = {opt_u_cost_fwd}")
+  print(f"  opt_x_cost_bwd        = {opt_x_cost_bwd}")
+  print(f"  opt_u_cost_bwd        = {opt_u_cost_bwd}")
+  print(f"  opt_cost_fwd          = {opt_cost_fwd}")
+  print(f"  opt_xT_fwd            = {opt_xT_fwd}")
+  print(f"  opt_x0_bwd            = {opt_x0_bwd}")
+  print(f"  ||x0 - opt_x0_bwd||^2 = {jp.sum((x0 - opt_x0_bwd)**2)}")
+  print()
 
   ### Set up the learned policy model.
   policy_init, policy = stax.serial(
@@ -106,33 +125,42 @@ def main():
   rng_init_params, rng = random.split(rng)
   _, init_policy_params = policy_init(rng_init_params, (x_dim, ))
   opt = make_optimizer(optimizers.adam(1e-3))(init_policy_params)
-  runny_run = jit(policy_integrate_cost(dynamics_fn, cost_fn, gamma, policy))
+  runny_run = jit(
+      policy_integrate_cost(dynamics_fn, position_cost_fn, control_cost_fn, gamma, policy))
 
   ### Main optimization loop.
-  cost_T_fwd_per_iter = []
+  x_cost_T_fwd_per_iter = []
+  u_cost_T_fwd_per_iter = []
   xT_fwd_per_iter = []
-  cost_0_bwd_per_iter = []
+  x_cost_0_bwd_per_iter = []
+  u_cost_0_bwd_per_iter = []
   x0_bwd_per_iter = []
 
   t1 = time.time()
   for i in range(5000):
     t0 = time.time()
     (y0_fwd, yT_fwd, y0_bwd), vjp = jax.vjp(runny_run, opt.value, x0, total_time)
-    cost_T_fwd, xT_fwd = yT_fwd
-    cost_0_bwd, x0_bwd = y0_bwd
+    x_cost_T_fwd, u_cost_T_fwd, xT_fwd = yT_fwd
+    x_cost_0_bwd, u_cost_0_bwd, x0_bwd = y0_bwd
 
-    yT_fwd_bar = (jp.ones(()), jp.zeros_like(x0))
+    yT_fwd_bar = (jp.ones(()), jp.ones(()), jp.zeros_like(x0))
     g, _, _ = vjp((zeros_like_tree(y0_fwd), yT_fwd_bar, zeros_like_tree(y0_bwd)))
     opt = opt.update(g)
 
-    cost_T_fwd_per_iter.append(cost_T_fwd)
+    x_cost_T_fwd_per_iter.append(x_cost_T_fwd)
+    u_cost_T_fwd_per_iter.append(u_cost_T_fwd)
     xT_fwd_per_iter.append(xT_fwd)
-    cost_0_bwd_per_iter.append(cost_0_bwd)
+    x_cost_0_bwd_per_iter.append(x_cost_0_bwd)
+    u_cost_0_bwd_per_iter.append(u_cost_0_bwd)
     x0_bwd_per_iter.append(x0_bwd)
 
     print(f"Episode {i}:")
-    print(f"  excess fwd cost = {cost_T_fwd - opt_cost_fwd}")
-    print(f"  bwd cost        = {cost_0_bwd}")
+    print(f"  excess fwd cost = {(x_cost_T_fwd + u_cost_T_fwd) - opt_cost_fwd}")
+    print(f"    excess fwd x cost = {x_cost_T_fwd - opt_x_cost_fwd}")
+    print(f"    excess fwd u cost = {u_cost_T_fwd - opt_u_cost_fwd}")
+    print(f"  bwd cost        = {x_cost_0_bwd + u_cost_0_bwd}")
+    print(f"    bwd x cost        = {x_cost_0_bwd}")
+    print(f"    bwd u cost        = {u_cost_0_bwd}")
     print(f"  bwd x0 - x0     = {x0_bwd - x0}")
     print(f"  fwd xT          = {xT_fwd}")
     print(f"  fwd xT norm sq. = {jp.sum(xT_fwd**2)}")
@@ -140,10 +168,15 @@ def main():
 
   print(f"total elapsed = {time.time() - t1}s")
 
-  cost_T_fwd_per_iter = jp.array(cost_T_fwd_per_iter)
+  x_cost_T_fwd_per_iter = jp.array(x_cost_T_fwd_per_iter)
+  u_cost_T_fwd_per_iter = jp.array(u_cost_T_fwd_per_iter)
   xT_fwd_per_iter = jp.array(xT_fwd_per_iter)
-  cost_0_bwd_per_iter = jp.array(cost_0_bwd_per_iter)
+  x_cost_0_bwd_per_iter = jp.array(x_cost_0_bwd_per_iter)
+  u_cost_0_bwd_per_iter = jp.array(u_cost_0_bwd_per_iter)
   x0_bwd_per_iter = jp.array(x0_bwd_per_iter)
+
+  cost_T_fwd_per_iter = x_cost_T_fwd_per_iter + u_cost_T_fwd_per_iter
+  cost_0_bwd_per_iter = x_cost_0_bwd_per_iter + u_cost_0_bwd_per_iter
 
   ### Plot performance per iteration, incl. average optimal policy performance.
   _, ax1 = plt.subplots()
@@ -151,7 +184,9 @@ def main():
   ax1.set_ylabel("Cost", color="tab:blue")
   ax1.set_yscale("log")
   ax1.tick_params(axis="y", labelcolor="tab:blue")
-  ax1.plot(cost_T_fwd_per_iter, color="tab:blue", label="Policy cost")
+  ax1.plot(cost_T_fwd_per_iter, color="tab:blue", label="Total rollout cost")
+  ax1.plot(x_cost_T_fwd_per_iter, linestyle="dotted", color="tab:blue", label="Position cost")
+  ax1.plot(u_cost_T_fwd_per_iter, linestyle="dashed", color="tab:blue", label="Control cost")
   plt.axhline(opt_cost_fwd, linestyle="--", color="gray")
   ax1.legend(loc="upper left")
 
@@ -159,11 +194,15 @@ def main():
   ax2.set_ylabel("Error", color="tab:red")
   ax2.set_yscale("log")
   ax2.tick_params(axis="y", labelcolor="tab:red")
-  ax2.plot(cost_0_bwd_per_iter**2, color="tab:red", label="Cost rewind error")
+  ax2.plot(cost_0_bwd_per_iter**2, alpha=0.5, color="tab:red", label="Cost rewind error")
   ax2.plot(jp.sum((x0_bwd_per_iter - x0)**2, axis=-1),
+           alpha=0.5,
            color="tab:purple",
            label="x(0) rewind error")
-  ax2.plot(jp.sum(xT_fwd_per_iter**2, axis=-1), color="tab:brown", label="x(T) squared norm")
+  ax2.plot(jp.sum(xT_fwd_per_iter**2, axis=-1),
+           alpha=0.5,
+           color="tab:brown",
+           label="x(T) squared norm")
   ax2.legend(loc="upper right")
 
   plt.title(f"ODE control of LQR problem")
