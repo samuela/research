d4b4add2da9bdb1a18805b24c04dce82ce43b781
Samuel Ainsworth <skainsworth@gmail.com>
2020-07-17 16:06:58 -0700

    backsolve checkpoints fixed and saving results works

--- julia/odecontrol/lqr/gradient_error_plots.jl
+++ julia/odecontrol/lqr/gradient_error_plots.jl
@@ -7,8 +7,10 @@ import DiffEqSensitivity:
     InterpolatingAdjoint, BacksolveAdjoint, QuadratureAdjoint, ODEAdjointProblem
 import LinearAlgebra: I, norm
 import Plots
-import PyPlot
+# import PyPlot
 import ControlSystems
+import ThreadPools: qmap
+import JLSO
 
 include("common.jl")
 
@@ -17,7 +19,7 @@ seed!(123)
 floatT = Float32
 x_dim = 2
 T = 10.0
-num_samples = 10
+num_samples = 5
 
 # num_hidden = 64
 # policy = FastChain(
@@ -30,19 +32,15 @@ num_samples = 10
 # A linear policy. Use this when operating with lqr_params below.
 policy = FastDense(x_dim, x_dim)
 
-# A = Matrix{floatT}(0 * I, x_dim, x_dim)
-# B = Matrix{floatT}(I, x_dim, x_dim)
-# Q = Matrix{floatT}(I, x_dim, x_dim)
-# R = Matrix{floatT}(I, x_dim, x_dim)
-
-const A = 0 * I
-const B = I
-const Q = I
-const R = I
+# About 10x faster than using I directly.
+const A = Matrix{floatT}(0 * I, x_dim, x_dim)
+const B = Matrix{floatT}(I, x_dim, x_dim)
+const Q = Matrix{floatT}(I, x_dim, x_dim)
+const R = Matrix{floatT}(I, x_dim, x_dim)
 dynamics, cost, sample_x0 = LinearEnv.linear_env(floatT, x_dim, A, B, Q, R)
 
 # lqr needs to be able to infer the dimension. See https://stackoverflow.com/questions/57270276/identity-matrix-in-julia.
-const K = ControlSystems.lqr(Matrix{Float64}(A, x_dim, x_dim), B, Q, R)
+const K = ControlSystems.lqr(A, B, Q, R)
 # We need to add zeros at the end for the bias term. See also https://github.com/SciML/DiffEqFlux.jl/blob/fa5d1678def49b0f0f3bad99e646f78c9a2a9d71/src/fast_layers.jl#L43.
 const lqr_params = vcat(convert(Array{floatT}, -K[:]), zeros(floatT, x_dim))
 
@@ -56,16 +54,14 @@ function cost_functional(x, policy_params, t)
 end
 
 # See https://github.com/SciML/DiffEqSensitivity.jl/issues/302 for context.
-dcost_tuple = (
+dcost_tuple = ((out, u, p, t) -> begin
+    ū, _, _ = Zygote.gradient(cost_functional, u, p, t)
+    out .= ū
+end,
     (out, u, p, t) -> begin
-        ū, _, _ = Zygote.gradient(cost_functional, u, p, t)
-        out .= ū
-    end,
-    (out, u, p, t) -> begin
-        _, p̄, _ = Zygote.gradient(cost_functional, u, p, t)
-        out .= p̄
-    end,
-)
+    _, p̄, _ = Zygote.gradient(cost_functional, u, p, t)
+    out .= p̄
+end,)
 
 function gold_standard_gradient(x0, policy_params)
     # Actual/gold standard evaluation. Using high-fidelity Vern9 method with
@@ -76,10 +72,10 @@ function gold_standard_gradient(x0, policy_params)
     fwd_sol = solve(
         ODEProblem(policy_dynamics!, x0, (0, T), policy_params),
         Vern9(),
-        u0 = x0_f64,
-        p = policy_params_f64,
-        abstol = 1e-14,
-        reltol = 1e-14,
+        u0=x0_f64,
+        p=policy_params_f64,
+        abstol=1e-14,
+        reltol=1e-14,
     )
     # Note that specifying dense = false is essential for getting acceptable
     # performance. save_everystep = false is another small win.
@@ -92,10 +88,10 @@ function gold_standard_gradient(x0, policy_params)
             dcost_tuple,
         ),
         Vern9(),
-        dense = false,
-        save_everystep = false,
-        abstol = 1e-14,
-        reltol = 1e-14,
+        dense=false,
+        save_everystep=false,
+        abstol=1e-14,
+        reltol=1e-14,
     )
     @assert typeof(fwd_sol.u) == Array{Array{Float64,1},1}
     @assert typeof(bwd_sol.u) == Array{Array{Float64,1},1}
@@ -109,10 +105,10 @@ function eval_interp(x0, policy_params, abstol, reltol)
     fwd_sol = solve(
         ODEProblem(policy_dynamics!, x0, (0, T), policy_params),
         Tsit5(),
-        u0 = x0,
-        p = policy_params,
-        abstol = abstol,
-        reltol = reltol,
+        u0=x0,
+        p=policy_params,
+        abstol=abstol,
+        reltol=reltol,
     )
     bwd_sol = solve(
         ODEAdjointProblem(
@@ -123,10 +119,10 @@ function eval_interp(x0, policy_params, abstol, reltol)
             dcost_tuple,
         ),
         Tsit5(),
-        dense = false,
-        save_everystep = false,
-        abstol = abstol,
-        reltol = reltol,
+        dense=false,
+        save_everystep=false,
+        abstol=abstol,
+        reltol=reltol,
     )
     @assert typeof(fwd_sol.u) == Array{Array{floatT,1},1}
     @assert typeof(bwd_sol.u) == Array{Array{floatT,1},1}
@@ -135,37 +131,35 @@ function eval_interp(x0, policy_params, abstol, reltol)
     # We do exactly as many f calls as there are function calls in the forward
     # pass, and in the backward pass we don't need to call f, but instead we
     # call ∇f.
-    (
-        fwd = fwd_sol,
+    (fwd = fwd_sol,
         bwd = bwd_sol,
         g = bwd_sol.u[end],
         nf = fwd_sol.destats.nf,
-        n∇f = bwd_sol.destats.nf,
-    )
+        n∇f = bwd_sol.destats.nf,)
 end
 
 function eval_backsolve(x0, policy_params, abstol, reltol, checkpointing)
     fwd_sol = solve(
         ODEProblem(policy_dynamics!, x0, (0, T), policy_params),
         Tsit5(),
-        u0 = x0,
-        p = policy_params,
-        abstol = abstol,
-        reltol = reltol,
+        u0=x0,
+        p=policy_params,
+        abstol=abstol,
+        reltol=reltol,
     )
     bwd_sol = solve(
         ODEAdjointProblem(
             fwd_sol,
-            BacksolveAdjoint(checkpointing = checkpointing),
+            BacksolveAdjoint(checkpointing=checkpointing),
             cost_functional,
             nothing,
             dcost_tuple,
         ),
         Tsit5(),
-        dense = false,
-        save_everystep = false,
-        abstol = abstol,
-        reltol = reltol,
+        dense=false,
+        save_everystep=false,
+        abstol=abstol,
+        reltol=reltol,
     )
     @assert typeof(fwd_sol.u) == Array{Array{floatT,1},1}
     @assert typeof(bwd_sol.u) == Array{Array{floatT,1},1}
@@ -174,13 +168,48 @@ function eval_backsolve(x0, policy_params, abstol, reltol, checkpointing)
     # reconstructed x state.
     # When running the backsolve adjoint we have additional f evaluations every
     # step of the backwards pass, since we need -f to reconstruct the x path.
-    (
-        fwd = fwd_sol,
+    (fwd = fwd_sol,
         bwd = bwd_sol,
-        g = bwd_sol.u[end][1:end-x_dim],
+        g = bwd_sol.u[end][1:end - x_dim],
         nf = fwd_sol.destats.nf + bwd_sol.destats.nf,
-        n∇f = bwd_sol.destats.nf,
+        n∇f = bwd_sol.destats.nf,)
+end
+
+function eval_euler_bptt(x0, policy_params)
+    fwd_sol = solve(
+        ODEProblem(policy_dynamics!, x0, (0, T), policy_params),
+        Tsit5(),
+        u0=x0,
+        p=policy_params,
+        abstol=abstol,
+        reltol=reltol,
     )
+    bwd_sol = solve(
+        ODEAdjointProblem(
+            fwd_sol,
+            BacksolveAdjoint(checkpointing=checkpointing),
+            cost_functional,
+            nothing,
+            dcost_tuple,
+        ),
+        Tsit5(),
+        dense=false,
+        save_everystep=false,
+        abstol=abstol,
+        reltol=reltol,
+    )
+    @assert typeof(fwd_sol.u) == Array{Array{floatT,1},1}
+    @assert typeof(bwd_sol.u) == Array{Array{floatT,1},1}
+
+    # In the backsolve adjoint, the last x_dim dimensions are for the
+    # reconstructed x state.
+    # When running the backsolve adjoint we have additional f evaluations every
+    # step of the backwards pass, since we need -f to reconstruct the x path.
+    (fwd = fwd_sol,
+        bwd = bwd_sol,
+        g = bwd_sol.u[end][1:end - x_dim],
+        nf = fwd_sol.destats.nf + bwd_sol.destats.nf,
+        n∇f = bwd_sol.destats.nf,)
 end
 
 # See https://github.com/SciML/DiffEqSensitivity.jl/issues/304 for why this
@@ -212,78 +241,100 @@ end
 #     # (fwd = fwd_sol, bwd = estbwd_sol, g = estbwd_sol.u[end][1:end-x_dim])
 # end
 
-function plot_results!(results, label)
-    nf_calls = [[sol.nf + sol.n∇f for sol in res] for res in results]
-    g_errors = [
-        [
-            norm(gold.bwd.u[end] - est.g)
-            for (gold, est) in zip(gold_standard_results, res)
-        ] for res in results
-    ]
-
-    function safe_error_bars(vs)
-        vs_median = map(median, vs)
-        vs_mean = map(mean, vs)
-        vs_std = map(std, vs)
-        collect(zip(
-            [
-                min(ṽ - 1e-6, σ + ṽ - μ)
-                for (ṽ, μ, σ) in zip(vs_median, vs_mean, vs_std)
-            ],
-            vs_std + vs_mean - vs_median,
-        ))
-    end
-
-    Plots.plot!(
-        map(median, nf_calls),
-        map(median, g_errors),
-        xlabel = "Function evaluations",
-        ylabel = "L2 error in the gradient",
-        xaxis = :log10,
-        yaxis = :log10,
-        xerror = safe_error_bars(nf_calls),
-        # yerror = safe_error_bars(g_errors),
-        label = label,
-    )
-end
-
 # Absolute error tolerances should generally be smaller than relative
 # tolerances.
-reltols = 0.1 .^ (0:9)
+reltols = 0.1.^(0:9)
 abstols = 1e-3 * reltols
 
 # init_conditions = [(sample_x0(), initial_params(policy)) for _ = 1:num_samples]
 init_conditions = [(sample_x0(), lqr_params) for _ = 1:num_samples]
 
-# These things are really, really slow on my computer
+# Must be before plot_results! since that depends on this...
 @info "gold standard"
+# @benchmark gold_standard_gradient(sample_x0(), lqr_params)
 @time gold_standard_results =
     [gold_standard_gradient(x0, θ) for (x0, θ) in init_conditions]
 
+force(thunk) = thunk()
+
 @info "interp"
-@time interp_results = [
-    [eval_interp(x0, θ, atol, rtol) for (x0, θ) in init_conditions] for (atol, rtol) in zip(abstols, reltols)
-]
+# @benchmark eval_interp(sample_x0(), lqr_params, 1e-12, 1e-9)
+@time interp_results = qmap(force, [
+    () -> (atol, rtol, eval_interp(x0, θ, atol, rtol))
+    for (x0, θ) in init_conditions
+    for (atol, rtol) in zip(abstols, reltols)
+])
+
 @info "backsolve"
-@time backsolve_results = [
-    [eval_backsolve(x0, θ, atol, rtol, false) for (x0, θ) in init_conditions] for (atol, rtol) in zip(abstols, reltols)
-]
+@time backsolve_results = qmap(force, [
+    () -> (atol, rtol, eval_backsolve(x0, θ, atol, rtol, false))
+    for (x0, θ) in init_conditions
+    for (atol, rtol) in zip(abstols, reltols)
+])
+
 @info "backsolve with checkpoints"
-@time backsolve_checkpointing_results = [
-    [eval_backsolve(x0, θ, atol, rtol, true) for (x0, θ) in init_conditions] for (atol, rtol) in zip(abstols, reltols)
-]
+@time backsolve_checkpointing_results = qmap(force, [
+    () -> (atol, rtol, eval_backsolve(x0, θ, atol, rtol, true))
+    for (x0, θ) in init_conditions
+    for (atol, rtol) in zip(abstols, reltols)
+])
 
 # The rest is useless...
 # quadrature_results = [
 #     [eval_quadrature(x0, θ, atol, rtol) for (x0, θ) in init_conditions] for (atol, rtol) in zip(abstols, reltols)
 # ]
 
-# Plots.pyplot()
-# Plots.PyPlotBackend()
-# pgfplotsx()
+JLSO.save("lqr_gradient_error_results.jlso",
+    :gold_standard_results => gold_standard_results,
+    :interp_results => interp_results,
+    :backsolve_results => backsolve_results,
+    :backsolve_checkpointing_results => backsolve_checkpointing_results)
 
-# Doing plot() clears the current figure.
-# Plots.plot(title = "Compute/accuracy tradeoff")
+# function plot_results!(results_flat, label)
+#     results = [[t for (a, r, t) in results_flat if (a, r) == (atol, rtol)]
+#                 for (atol, rtol) in zip(abstols, reltols)]
+
+#     nf_calls = [[sol.nf + sol.n∇f for sol in res] for res in results]
+#     g_errors = [
+#         [
+#             norm(gold.bwd.u[end] - est.g)
+#             for (gold, est) in zip(gold_standard_results, res)
+#         ] for res in results
+#     ]
+
+#     function safe_error_bars(vs)
+#         vs_median = map(median, vs)
+#         vs_mean = map(mean, vs)
+#         vs_std = map(std, vs)
+#         collect(zip(
+#             [
+#                 min(ṽ - 1e-12, σ + ṽ - μ)
+#                 for (ṽ, μ, σ) in zip(vs_median, vs_mean, vs_std)
+#             ],
+#             vs_std + vs_mean - vs_median,
+#         ))
+#     end
+
+#     Plots.plot!(
+#         map(median, nf_calls),
+#         map(median, g_errors),
+#         xlabel="Function evaluations",
+#         ylabel="L2 error in the gradient",
+#         xaxis=:log10,
+#         yaxis=:log10,
+#         # xerror=safe_error_bars(nf_calls),
+#         yerror=safe_error_bars(g_errors),
+#         label=label,
+#     )
+# end
+
+# # Plots.pyplot()
+# # Plots.PyPlotBackend()
+# # pgfplotsx()
+
+# # Doing plot() clears the current figure.
+# Plots.plot(title="Compute/accuracy tradeoff")
 # plot_results!(backsolve_results, "Neural ODE")
 # plot_results!(backsolve_checkpointing_results, "Neural ODE with checkpointing")
 # plot_results!(interp_results, "Ours")
+# Plots.savefig("lqr_tradeoff.pdf")
