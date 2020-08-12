d4e40c1b36d8c31c7d819655dac89f1020dbdd64
Samuel Ainsworth <skainsworth@gmail.com>
2020-08-12 16:34:35 -0700

    introduce PyCall and setup taichi

---
+++ julia/odecontrol/deps/build.jl
@@ -0,0 +1,12 @@
+"""PyCall has its own little Python install, and we need to make sure that we
+install all the right dependencies in that environment. Note that you need to
+restart Julia after running this in order to be able to @pyimport the installed
+packages. Run this with `] build`.
+
+See https://gist.github.com/Luthaf/368a23981c8ec095c3eb."""
+
+import PyCall: @pyimport
+
+@pyimport pip
+const PIP_PACKAGES = ["taichi"]
+pip.main(["install", "--user", PIP_PACKAGES...])
---
+++ julia/odecontrol/difftaichi/mass_spring.jl
@@ -0,0 +1,3 @@
+import PyCall: @pyimport
+
+@pyimport taichi
