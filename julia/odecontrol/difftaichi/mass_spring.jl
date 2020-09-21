d84ec7f9dbc638940046f3057bbb27fc1b9e651c
Samuel Ainsworth <skainsworth@gmail.com>
2020-09-20 22:39:31 -0700

    ok difftaichi gradients seem to be working!

--- julia/odecontrol/difftaichi/mass_spring.jl
+++ julia/odecontrol/difftaichi/mass_spring.jl
@@ -23,7 +23,6 @@ import sys
 sys.path.insert(0, "./difftaichi/python")
 """
 
-# @pyimport taichi as ti
 np = pyimport("numpy")
 importlib = pyimport("importlib")
 # For some reason, @pyimport doesn't work with module reloading.
@@ -59,6 +58,8 @@ n_springs = size(springs, 1)
 # We go back and forth between flat and non-flat representations for x and v. These flattened representations are
 # column-major since that's how Julia does things. Note that Numpy is row-major by default however!
 
+import Zygote
+
 # Specifying Array types here is important for ReverseDiff.jl to be able to hit the tracked version of this function
 # below. Why is this the case? Who knows. Because x and v are actually ReshapedArrays, it ends up being easier to
 # dispatch on u. See https://github.com/JuliaDiff/ReverseDiff.jl/issues/150#issuecomment-692649803.
@@ -69,6 +70,16 @@ function forces_fn(x, v, u::ReverseDiff.TrackedArray)
     ReverseDiff.track(forces_fn, x, v, u)
 end
 ReverseDiff.@grad function forces_fn(x, v, u)
+    # We reuse these for both the forward and backward.
+    x_np = np.array(x, dtype=np.float32)
+    v_np = np.array(v, dtype=np.float32)
+    u_np = np.array(u, dtype=np.float32)
+    function pullback(ΔΩ)
+        mass_spring.forces_fn_vjp(x_np, v_np, u_np, np.array(ΔΩ, dtype=np.float32))
+    end
+    mass_spring.forces_fn(x_np, v_np, u_np), pullback
+end
+Zygote.@adjoint function forces_fn(x, v, u)
     # We reuse these for both the forward and backward.
     x_np = np.array(x, dtype=np.float32)
     v_np = np.array(v, dtype=np.float32)
@@ -92,7 +103,8 @@ begin
         local g = randn(Float32, (n_objects, 2))
         f(x, v, u) = sum(forces_fn(x, v, u) .* g)
         local x̄1, v̄1, ū1 = FiniteDifferences.grad(FiniteDifferences.central_fdm(3, 1), f, x, v, u)
-        local x̄2, v̄2, ū2 = ReverseDiff.gradient(f, (x, v, u))
+        # local x̄2, v̄2, ū2 = ReverseDiff.gradient(f, (x, v, u))
+        local x̄2, v̄2, ū2 = Zygote.gradient(f, x, v, u)
         @test isapprox(x̄1, x̄2; rtol=1e-3)
         @test maximum(abs.(v̄1 - v̄2)) <= 1e-2
         @test isapprox(ū1, ū2, rtol = 1e-2)
@@ -111,11 +123,27 @@ function dynamics(state, u)
     # Positions are [x, y]. The ground has infinite friction in the difftaichi model.
     # We need to do this in order to work with ReverseDiff.jl since a for-loop requires setindex! which isn't allowed in
     # AD code.
-    v_acc_x = [if x[i, 2] > ground_height v_acc[i, 1] else 0 end for i in 1:n_objects]
-    v_acc_y = [if x[i, 2] > ground_height v_acc[i, 2] else max(0, v_acc[i, 2]) end for i in 1:n_objects]
+    v_acc_x = [if x[i, 2] > ground_height v_acc[i, 1] else 0.0 end for i in 1:n_objects]
+    v_acc_y = [if x[i, 2] > ground_height v_acc[i, 2] else max(0.0, v_acc[i, 2]) end for i in 1:n_objects]
 
     # We need to flatten everything back down to a vector. Reuse v_flat == v[:].
     [v_flat; v_acc_x; v_acc_y]
+end
+
+begin
+    @info "Testing dynamics gradients"
+    seed!(123)
+    for i in 1:10
+        local state = 0.01 * randn(Float32, (4 * n_objects, ))
+        local u = 0.01 * randn(Float32, (n_springs, ))
+        local g = 0.01 * randn(Float32, (4 * n_objects, ))
+        f(state, u) = sum(dynamics(state, u) .* g)
+        local g_state_fd, g_u_fd = FiniteDifferences.grad(FiniteDifferences.central_fdm(5, 1), f, state, u)
+        # local g_state_rd, g_u_rd = ReverseDiff.gradient(f, (state, u))
+        local g_state_rd, g_u_rd = Zygote.gradient(f, state, u)
+        @test isapprox(g_state_fd, g_state_rd; rtol=1e-1)
+        @test maximum(abs.(g_u_fd - g_u_rd)) <= 1e-1
+    end
 end
 
 function cost(state, u)
@@ -149,6 +177,20 @@ function observation(state, t)
     [periodic_signal; center[:]; offsets[:]]
 end
 
+begin
+    @info "Testing observation gradients"
+    seed!(123)
+    for i in 1:10
+        local state = 0.1 * randn(Float32, (4 * n_objects, ))
+        local t = randn(Float32)
+        local g = randn(Float32, (n_sin_waves + 2 + 2 * n_objects, ))
+        f(state, t) = sum(observation(state, t) .* g)
+        local g_state_fd, _ = FiniteDifferences.grad(FiniteDifferences.central_fdm(5, 1), f, state, t)
+        local g_state_rd, _ = Zygote.gradient(f, state, t)
+        @test maximum(abs.(g_state_fd - g_state_rd)) <= 1e-3
+    end
+end
+
 ################
 
 callback = VectorContinuousCallback(
@@ -169,6 +211,7 @@ callback = VectorContinuousCallback(
         integrator.u[1+3*n_objects+idx] = 0
     end,
     n_objects,
+    abstol=1e-2
 )
 
 # Difftaichi trains for 2048 // 3 steps, each being 0.004s long. That all works out to 2.728s.
@@ -190,85 +233,88 @@ learned_policy_goodies =
     sample_x0(),
     init_policy_params,
     Tsit5(),
-    Dict(:callback => callback, :rtol => 1e-3, :atol => 1e-3, :dt => 0.004),
+    Dict(:callback => callback, :rtol => 1e-3, :atol => 1e-3),
+    # Dict(:callback => callback, :rtol => 1e-3, :atol => 1e-3, :dt => 0.004),
 )
-@time stuff = pullback(ones(1 + size(sample_x0(), 1)), InterpolatingAdjoint(autojacvec=ReverseDiffVJP()))
+@time stuff = pullback(ones(1 + size(sample_x0(), 1)), InterpolatingAdjoint(autojacvec=ZygoteVJP()))
+
+batch_size = 1
 
 # Train
-# function run(loss_and_grad)
-#     # Seed here so that both interp and euler get the same batches.
-#     seed!(123)
+function run(loss_and_grad)
+    # Seed here so that both interp and euler get the same batches.
+    seed!(123)
 
-#     loss_per_iter = fill(NaN, num_iters)
-#     policy_params_per_iter = fill(NaN, num_iters, length(init_policy_params))
-#     g_per_iter = fill(NaN, num_iters, length(init_policy_params))
-#     nf_per_iter = fill(NaN, num_iters)
-#     n∇ₓf_per_iter = fill(NaN, num_iters)
-#     n∇ᵤf_per_iter = fill(NaN, num_iters)
+    loss_per_iter = fill(NaN, num_iters)
+    policy_params_per_iter = fill(NaN, num_iters, length(init_policy_params))
+    g_per_iter = fill(NaN, num_iters, length(init_policy_params))
+    nf_per_iter = fill(NaN, num_iters)
+    n∇ₓf_per_iter = fill(NaN, num_iters)
+    n∇ᵤf_per_iter = fill(NaN, num_iters)
 
-#     policy_params = deepcopy(init_policy_params)
-#     batches = [[sample_x0() for _ = 1:batch_size] for _ = 1:num_iters]
-#     # opt = ADAM()
-#     opt = Momentum(0.001)
-#     # opt = Optimiser(ExpDecay(0.001, 0.5, 1000, 1e-5), Momentum(0.001))
-#     # opt =LBFGS(
-#     #     alphaguess = LineSearches.InitialStatic(alpha = 0.001),
-#     #     linesearch = LineSearches.Static(),
-#     # )
-#     progress = ProgressMeter.Progress(num_iters)
-#     for iter = 1:num_iters
-#         loss, g, info = loss_and_grad(batches[iter], policy_params)
-#         loss_per_iter[iter] = loss
-#         policy_params_per_iter[iter, :] = policy_params
-#         g_per_iter[iter, :] = g
-#         nf_per_iter[iter] = info.nf
-#         n∇ₓf_per_iter[iter] = info.n∇ₓf
-#         n∇ᵤf_per_iter[iter] = info.n∇ᵤf
+    policy_params = deepcopy(init_policy_params)
+    batches = [[sample_x0() for _ = 1:batch_size] for _ = 1:num_iters]
+    # opt = ADAM()
+    opt = Momentum(0.001)
+    # opt = Optimiser(ExpDecay(0.001, 0.5, 1000, 1e-5), Momentum(0.001))
+    # opt =LBFGS(
+    #     alphaguess = LineSearches.InitialStatic(alpha = 0.001),
+    #     linesearch = LineSearches.Static(),
+    # )
+    progress = ProgressMeter.Progress(num_iters)
+    for iter = 1:num_iters
+        loss, g, info = loss_and_grad(batches[iter], policy_params)
+        loss_per_iter[iter] = loss
+        policy_params_per_iter[iter, :] = policy_params
+        g_per_iter[iter, :] = g
+        nf_per_iter[iter] = info.nf
+        n∇ₓf_per_iter[iter] = info.n∇ₓf
+        n∇ᵤf_per_iter[iter] = info.n∇ᵤf
 
-#         clamp!(g, -10, 10)
-#         Flux.Optimise.update!(opt, policy_params, g)
-#         ProgressMeter.next!(
-#             progress;
-#             showvalues = [
-#                 (:iter, iter),
-#                 (:loss, loss),
-#                 (:nf, info.nf / batch_size),
-#                 (:n∇ₓf, info.n∇ₓf / batch_size),
-#                 (:n∇ᵤf, info.n∇ᵤf / batch_size),
-#             ],
-#         )
-#     end
+        # clamp!(g, -10, 10)
+        Flux.Optimise.update!(opt, policy_params, g)
+        ProgressMeter.next!(
+            progress;
+            showvalues = [
+                (:iter, iter),
+                (:loss, loss),
+                (:nf, info.nf / batch_size),
+                (:n∇ₓf, info.n∇ₓf / batch_size),
+                (:n∇ᵤf, info.n∇ᵤf / batch_size),
+            ],
+        )
+    end
 
-#     (
-#         loss_per_iter = loss_per_iter,
-#         policy_params_per_iter = policy_params_per_iter,
-#         g_per_iter = g_per_iter,
-#         nf_per_iter = nf_per_iter,
-#         n∇ₓf_per_iter = n∇ₓf_per_iter,
-#         n∇ᵤf_per_iter = n∇ᵤf_per_iter,
-#     )
-# end
+    (
+        loss_per_iter = loss_per_iter,
+        policy_params_per_iter = policy_params_per_iter,
+        g_per_iter = g_per_iter,
+        nf_per_iter = nf_per_iter,
+        n∇ₓf_per_iter = n∇ₓf_per_iter,
+        n∇ᵤf_per_iter = n∇ᵤf_per_iter,
+    )
+end
 
-# @info "InterpolatingAdjoint"
-# interp_results = run(
-#     (x0_batch, θ) -> learned_policy_goodies.ez_loss_and_grad_many(
-#         x0_batch,
-#         θ,
-#         Tsit5(),
-#         InterpolatingAdjoint(autojacvec = ReverseDiffVJP(true)),
-#         fwd_solve_kwargs = Dict(:callback => callback, :rtol => 1e-3, :atol => 1e-3),
-#     ),
-# )
+@info "InterpolatingAdjoint"
+interp_results = run(
+    (x0_batch, θ) -> learned_policy_goodies.ez_loss_and_grad_many(
+        x0_batch,
+        θ,
+        Tsit5(),
+        InterpolatingAdjoint(autojacvec = ZygoteVJP()),
+        fwd_solve_kwargs = Dict(:callback => callback, :rtol => 1e-3, :atol => 1e-3),
+    ),
+)
 
 # Visualize a rollout:
-# ts = 0:0.01:T
-# zs = fwd_sol.(ts)
-# xs_flat = [z[2:end] for z in zs]
-# xs = [reshape(z[1:2*n_objects], (n_objects,2)) for z in xs_flat]
-# acts = [policy(observation(x, t), init_policy_params) for (x, t) in zip(xs_flat, ts)]
+ts = 0:0.01:T
+zs = fwd_sol.(ts)
+xs_flat = [z[2:end] for z in zs]
+xs = [reshape(z[1:2*n_objects], (n_objects,2)) for z in xs_flat]
+acts = [policy(observation(x, t), interp_results.policy_params_per_iter[end, :]) for (x, t) in zip(xs_flat, ts)]
 
 # This doesn't work on headless machines.
-# mass_spring.animate(xs, acts, ground_height, output = "poopypoops")
+mass_spring.animate(xs, acts, ground_height, output = "poopypoops")
 
 # mass_spring2 = pyimport("mass_spring2")
 # mass_spring2.main(1)
