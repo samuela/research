0741ff0d12ffe1cd90b8271eb396f7e3c23c05c4
Samuel Ainsworth <skainsworth@gmail.com>
2020-09-26 14:15:09 -0700

    not working yet, checkpoint to track down a minimal reproduction

--- julia/odecontrol/difftaichi/ppg_toi.jl
+++ julia/odecontrol/difftaichi/ppg_toi.jl
@@ -1,5 +1,5 @@
 import DiffEqBase
-import DiffEqBase: DiscreteCallback, CallbackSet
+import DiffEqBase: DiscreteCallback, DynamicalODEProblem
 import DiffEqSensitivity:
     solve,
     ODEProblem,
@@ -7,6 +7,7 @@ import DiffEqSensitivity:
     InterpolatingAdjoint,
     AdjointSensitivityIntegrand
 import Zygote
+import RecursiveArrayTools: ArrayPartition
 
 struct TOIStuff
     conditions
@@ -22,6 +23,7 @@ end
 
 function (sol::TOISolution)(t)
     @assert sol.solutions[1].t[1] <= t <= sol.solutions[end].t[end]
+    # TODO: what does this do in the cases where t < 0 and t > T?
     ix = searchsortedlast([s.t[1] for s in sol.solutions], t)
     subsol = sol.solutions[ix]
     # There is some small 2 * time_epsilon sized interval after the solution stops, that t may fall into. In that case
@@ -33,23 +35,42 @@ function (sol::TOISolution)(t)
     end
 end
 
-function ppg_toi_goodies(dynamics, cost, policy, toi, T)
-    function aug_dynamics(z, policy_params, t)
-        x = @view z[2:end]
-        u = policy(x, t, policy_params)
-        [cost(x, u); dynamics(x, u)]
+function ppg_toi_goodies(v_dynamics, x_dynamics, cost, policy, toi, T)
+    # x and v are ArrayPartitions(cost::Real, 2-d Array).
+    # Our trick for shoehorning cost accumulation into a DynamicalODEProblem is to include it in both x and v. Then we
+    # update it in the aug_dyn_x dynamics. The space for it in v is unused and left at zero.
+    aug_dyn_v(v_ap, x_ap, policy_params, t) = begin
+        # TODO this might be the problem. Need to isolate minimal reproduction.
+        v = v_ap.x[2]
+        x = x_ap.x[2]
+        # Don't know the original shapes of x, v which makes this a bit difficult.
+        # v = reshape(@view(v_ap[2:end]), (:, 2))
+        # x = reshape(@view(x_ap[2:end]), (:, 2))
+        u = policy(x, v, policy_params, t)
+        ArrayPartition(zeros(), v_dynamics(v, x, u))
+    end
+    aug_dyn_x(v_ap, x_ap, policy_params, t) = begin
+        v = v_ap.x[2]
+        x = x_ap.x[2]
+        # v = reshape(@view(v_ap[2:end]), (:, 2))
+        # x = reshape(@view(x_ap[2:end]), (:, 2))
+        u = policy(x, v, policy_params, t)
+        # See https://github.com/FluxML/Zygote.jl/issues/800.
+        ArrayPartition(fill(cost(v, x, u)), x_dynamics(v, x, u))
     end
 
-    callback = DiscreteCallback((u, t, integrator) -> true, (integrator) -> begin
+    callback = DiscreteCallback((_, _, _) -> true, (integrator) -> begin
         # In some cases (eg toi_test6.jl) there is a zero-crossing, but the integrators are sufficiently smart as to
         # skip it entirely. Ie, you cross zero twice and so you would never notice without some more careful testing.
         test_times = range(integrator.tprev, integrator.t, length=10)
-        xs = [z[2:end] for z in integrator.(test_times)]
+        # This is an Array of ArrayPartition(ArrayPartition(cost, v), ArrayPartition(cost, x)).
+        test_vals = integrator.(test_times)
+
         # There's a little tiny droplet of optimization that could be squeezed out here. We could be lazier.
         # For each condition, find the index of the time span with the first down-crossing. `nothing` if no
         # down-crossing is found.
         ixs = filter((a) -> !isnothing(a[1]), [begin
-            cs = condition.(xs)
+            cs = [condition(vx_aug.x[1].x[2], vx_aug.x[2].x[2]) for vx_aug in test_vals]
             # Only fire for down-crossings: positive -> negative.
             (findfirst((j) -> cs[j] > 0 > cs[j + 1], 1:(length(cs) - 1)), i)
         end for (i, condition) in enumerate(toi.conditions)])
@@ -59,8 +80,12 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
 
             # See https://github.com/SciML/DiffEqBase.jl/blob/d4973e21ff31dc1d355e84ae2b4c1d3c9546b6b2/src/callbacks.jl#L673.
             event_t = DiffEqBase.bisection(
-                (t) -> toi.conditions[condition_ix](integrator(t)[2:end]),
-                (test_times[t_ix], test_times[t_ix + 1]), isone(integrator.tdir)
+                (t) -> begin
+                    vx_aug = integrator(t)
+                    toi.conditions[condition_ix](vx_aug.x[1].x[2], vx_aug.x[2].x[2])
+                end,
+                (test_times[t_ix], test_times[t_ix + 1]),
+                isone(integrator.tdir)
             )
 
             # If, on the off chance, event_t - time_epsilon < tprev, this will return an error. Soluton is
@@ -73,7 +98,7 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
 
     # See https://discourse.julialang.org/t/why-the-separation-of-odeproblem-and-solve-in-differentialequations-jl/43737
     # for a discussion of the performance of the pullbacks.
-    function loss_pullback(x0, policy_params, solvealg, solve_kwargs)
+    function loss_pullback(v0, x0, policy_params, solvealg, solve_kwargs)
         # So we need to do some weird hacks to get correct gradients when
         # dealing with events. DiffEqSensitivity does not return the correct
         # gradients when using ContinuousCallbacks. Instead we manually detect
@@ -82,16 +107,25 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
         # working properly.
         done = false
         current_t = 0.0
-        current_z = vcat(0.0, x0)
+        current_vx = ArrayPartition(
+            ArrayPartition(zeros(), v0),
+            ArrayPartition(zeros(), x0))
         tape = []
         while !done
             # There's some very small chance that we TOI jump past the final time T. I don't want to find out what
             # DifferentialEquations.jl does in that case.
             @assert current_t < T
+
             fwd_sol = solve(
-                ODEProblem(aug_dynamics, current_z, (current_t, T), policy_params),
+                DynamicalODEProblem(
+                    aug_dyn_v,
+                    aug_dyn_x,
+                    current_vx.x[1],
+                    current_vx.x[2],
+                    (current_t, T),
+                    policy_params
+                ),
                 solvealg,
-                u0 = current_z,
                 p = policy_params;
                 callback = callback,
                 solve_kwargs...,
@@ -99,8 +133,16 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
 
             if fwd_sol.retcode == :Terminated
                 current_t = fwd_sol.t[end] + 2 * toi.time_epsilon
-                current_z[:], toi_pullback = Zygote.pullback(
-                    (z) -> [z[1]; toi.affect(z[2:end], 2 * toi.time_epsilon)],
+                current_vx[:], toi_pullback = Zygote.pullback(
+                    (vx) -> begin
+                        v = vx.x[1].x[2]
+                        x = vx.x[2].x[2]
+                        accum_cost = vx.x[2].x[1]
+                        new_v, new_x = toi.affect(v, x, 2 * toi.time_epsilon)
+                        ArrayPartition(
+                            ArrayPartition(zeros(), new_v),
+                            ArrayPartition(accum_cost, new_x))
+                    end,
                     fwd_sol[end])
                 push!(tape, (fwd_sol, toi_pullback))
             else
@@ -137,6 +179,7 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
                     save_everystep = false,
                     save_start = false,
                 )
+                @error "yay bwd_sol"
                 # We do exactly as many f calls as there are function calls in the
                 # forward pass, and in the backward pass we don't need to call f,
                 # but instead we call ∇f.
@@ -163,8 +206,5 @@ function ppg_toi_goodies(dynamics, cost, policy, toi, T)
         TOISolution(map(first, tape)), pullback
     end
 
-    (
-        aug_dynamics = aug_dynamics,
-        loss_pullback = loss_pullback,
-    )
+    loss_pullback
 end
--- julia/odecontrol/difftaichi/toi_test1.jl
+++ julia/odecontrol/difftaichi/toi_test1.jl
@@ -3,26 +3,48 @@
 include("ppg_toi.jl")
 
 import DifferentialEquations: Tsit5
+import UnicodePlots: lineplot
 
-dynamics(state, u) = [state[2], 0]
-cost(state, u) = 0
-policy(state, t, p) = 0
-toi_affect(state, dt) = [-dt * state[2] - state[1], -state[2]]
+v_dynamics(v, x, u) = [0.0]
+x_dynamics(v, x, u) = v
+cost(v, x, u) = 0.0
+policy(v, x, p, t) = 0.0
+toi_affect(v, x, dt) = (-v, -dt * v - x)
 terminal_cost(state) = (state[1] - 0.6) ^ 2
 
-goodies = ppg_toi_goodies(dynamics, cost, policy, TOIStuff([(x) -> x[1]], toi_affect, 1e-3), 1.0)
+T = 1.0
+# goodies = ppg_toi_goodies(dynamics, cost, policy, TOIStuff([(x) -> x[1]], toi_affect, 1e-3), 1.0)
+loss_pullback = ppg_toi_goodies(
+    v_dynamics,
+    x_dynamics,
+    cost,
+    policy,
+    TOIStuff([(v, x) -> x[1]], toi_affect, 1e-3),
+    # TOIStuff([], toi_affect, 1e-3),
+    T)
 
-x0 = [0.75, -1]
-for _ in 1:100
-    sol, pb1 = goodies.loss_pullback(x0, zeros(1), nothing, Dict())
-    xT = sol.solutions[end].u[end][2:end]
+v0 = [-1.0]
+x0 = [0.75]
+
+sol, _ = loss_pullback(v0, x0, zeros(), nothing, Dict())
+
+ts = 0:0.01:T
+lineplot(ts, [z.x[2].x[2][1] for z in sol.(ts)], title = "x") |> show
+lineplot(ts, [z.x[1].x[2][1] for z in sol.(ts)], title = "v") |> show
+
+@assert length(sol.solutions) == 2
+
+for _ in 1:1
+    local sol, pb1 = loss_pullback(v0, x0, zeros(), nothing, Dict())
+    xT = sol.solutions[end].u[end].x[2].x[2]
     local cost, pb2 = Zygote.pullback(terminal_cost, xT)
 
     (g_xT, ) = pb2(1.0)
-    local g = [0; g_xT]
+    local g = ArrayPartition(ArrayPartition(zeros(), zero(v0)), ArrayPartition(zeros(), g_xT))
     g = pb1(g, InterpolatingAdjoint()).g_z0
+    @error "yay"
     g = [g[2], 0]
 
     global x0 -= 0.1 * g
 end
-@assert x0 ≈ [0.4, -1.0]
+# @assert x0 ≈ [0.4, -1.0]
