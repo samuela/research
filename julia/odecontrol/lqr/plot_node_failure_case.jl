80a203c7468641007657210e4e1aced152259f68
Sam Ainsworth <skainswo@zh-ws1.cs.washington.edu>
2020-11-19 21:29:54 -0800

    more polish, and refactor

--- julia/odecontrol/lqr/plot_node_failure_case.jl
+++ julia/odecontrol/lqr/plot_node_failure_case.jl
@@ -17,7 +17,7 @@ include("../ppg.jl")
 seed!(123)
 
 rcParams = PyDict(PyPlot.matplotlib["rcParams"])
-rcParams["font.size"] = 16
+rcParams["font.size"] = 20
 rcParams["figure.figsize"] = (8, 4)
 
 floatT = Float32
@@ -47,7 +47,7 @@ lqr_loss = lqr_sol[end][1]
 
 learned_policy_goodies = ppg_goodies(dynamics, cost, (x, t, θ) -> policy(x, θ), T)
 
-function plot_neural_ode()
+function run_neural_ode()
     policy_params = deepcopy(init_policy_params)
     learned_loss_per_iter = fill(NaN, num_iters)
     reconst_error_per_iter = fill(NaN, num_iters)
@@ -64,30 +64,11 @@ function plot_neural_ode()
         end
     end
 
-    begin
-        _, ax1 = PyPlot.subplots()
-        ax1.set_xlabel("Iteration")
-        ax1.set_ylabel("Loss \$\\mathcal{L}(x_0, \\theta)\$", color = "tab:blue")
-        ax1.tick_params(axis = "y", labelcolor = "tab:blue")
-        # ax1.set_yscale("log")
-        ax1.plot(learned_loss_per_iter, color = "tab:blue")
-        PyPlot.axhline(lqr_loss, linestyle = "--", color = "grey")
-
-        ax2 = ax1.twinx()
-        ax2.set_ylabel("L2 error", color = "tab:red")
-        ax2.tick_params(axis = "y", labelcolor = "tab:red")
-        ax2.plot([], color = "tab:blue", label = "Neural ODE solution")
-        ax2.plot([], linestyle = "--", color = "grey", label = "LQR solution")
-        ax2.plot(reconst_error_per_iter, color = "tab:red", label = "Backsolve error")
-
-        PyPlot.legend(loc = "upper left")
-        PyPlot.tight_layout()
-        PyPlot.savefig("node_comparison.pdf")
-    end
+    learned_loss_per_iter, reconst_error_per_iter
 end
 
 ################################################################################
-function plot_quad()
+function run_ctpg()
     policy_params = deepcopy(init_policy_params)
     learned_loss_per_iter = fill(NaN, num_iters)
     opt = ADAM()
@@ -103,37 +84,64 @@ function plot_quad()
         end
     end
 
-    begin
-        _, ax1 = PyPlot.subplots()
-        ax1.set_xlabel("Iteration")
-        ax1.set_ylabel("Loss \$\\mathcal{L}(x_0, \\theta)\$", color = "tab:blue")
-        ax1.tick_params(axis = "y", labelcolor = "tab:blue")
-        # ax1.set_yscale("log")
-        ax1.plot(learned_loss_per_iter, color = "tab:blue")
-        PyPlot.axhline(lqr_loss, linestyle = "--", color = "grey")
-
-        ax2 = ax1.twinx()
-        ax2.set_ylabel("L2 error", color = "tab:red")
-        ax2.tick_params(axis = "y", labelcolor = "tab:red")
-        ax2.plot([], color = "tab:blue", label = "CTPG (ours)")
-        ax2.plot([], linestyle = "--", color = "grey", label = "LQR solution")
-
-        # Interpolated forward pass has no reconstruction error since we have a
-        # knot point at x(0)!
-        ax2.plot(
-            zeros(size(learned_loss_per_iter)),
-            color = "tab:red",
-            label = "Backsolve error",
-        )
-        ax2.set_ylim(-1.0, 50.0)
-
-        PyPlot.legend(loc = "upper left")
-        PyPlot.tight_layout()
-        PyPlot.savefig("ours_comparison.pdf")
-    end
+    learned_loss_per_iter
 end
 
 @info "Neural ODE"
-plot_neural_ode()
-@info "PPG"
-plot_quad()
+# node_learned_loss_per_iter, node_reconst_error_per_iter = run_neural_ode()
+
+@info "CTPG"
+# ctpg_learned_loss_per_iter = run_ctpg()
+
+begin
+    _, ax1 = PyPlot.subplots()
+    ax1.set_xlabel("Iteration")
+    ax1.set_ylabel("Loss \$\\mathcal{L}(x_0, \\theta)\$", color = "tab:blue")
+    ax1.tick_params(axis = "y", labelcolor = "tab:blue")
+    # ax1.set_yscale("log")
+    ax1.plot(node_learned_loss_per_iter, color = "tab:blue")
+    PyPlot.axhline(lqr_loss, linestyle = "--", color = "grey")
+    ax1.set_ylim(-5, 55)
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("L2 error", color = "tab:red")
+    ax2.tick_params(axis = "y", labelcolor = "tab:red")
+    ax2.plot([], color = "tab:blue", label = "Neural ODE solution")
+    ax2.plot([], linestyle = "--", color = "grey", label = "LQR solution")
+    ax2.plot(node_reconst_error_per_iter, color = "tab:red", label = "Backsolve error")
+    ax2.set_ylim(-5e5, 8e6)
+
+    PyPlot.legend(loc = "upper left")
+    PyPlot.tight_layout()
+    PyPlot.savefig("node_comparison.pdf")
+end
+
+begin
+    _, ax1 = PyPlot.subplots()
+    ax1.set_xlabel("Iteration")
+    ax1.set_ylabel("Loss \$\\mathcal{L}(x_0, \\theta)\$", color = "tab:blue")
+    ax1.tick_params(axis = "y", labelcolor = "tab:blue")
+    # ax1.set_yscale("log")
+    ax1.plot(ctpg_learned_loss_per_iter, color = "tab:blue")
+    PyPlot.axhline(lqr_loss, linestyle = "--", color = "grey")
+    ax1.set_ylim(-5, 55)
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("L2 error", color = "tab:red")
+    ax2.tick_params(axis = "y", labelcolor = "tab:red")
+    ax2.plot([], color = "tab:blue", label = "CTPG (ours)")
+    ax2.plot([], linestyle = "--", color = "grey", label = "LQR solution")
+
+    # Interpolated forward pass has no reconstruction error since we have a
+    # knot point at x(0)!
+    ax2.plot(
+        zeros(size(ctpg_learned_loss_per_iter)),
+        color = "tab:red",
+        label = "Backsolve error",
+    )
+    ax2.set_ylim(-5e5, 8e6)
+
+    PyPlot.legend(loc = "upper right")
+    PyPlot.tight_layout()
+    PyPlot.savefig("ours_comparison.pdf")
+end
