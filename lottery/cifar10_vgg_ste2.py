a0665fb21b2f12f1a528a9172df549d55a7fcc1c
Samuel Ainsworth <skainsworth@gmail.com>
2022-06-18 03:37:51 +0000

    lottery: add cifar10_vgg_ste2.py

---
+++ lottery/cifar10_vgg_ste2.py
@@ -0,0 +1,461 @@
+import argparse
+import pickle
+from collections import defaultdict
+from pathlib import Path
+from typing import NamedTuple
+
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import optax
+import tensorflow as tf
+from flax.core import freeze
+from flax.serialization import from_bytes
+from flax.training.train_state import TrainState
+from jax import jit, random, tree_map, value_and_grad
+from jax.lax import stop_gradient
+from jax.tree_util import tree_reduce
+from scipy.optimize import linear_sum_assignment
+from tqdm import tqdm
+
+import wandb
+from cifar10_vgg_run import (init_train_state, load_datasets, make_stuff, make_vgg_width_ablation)
+from utils import (ec2_get_instance_type, flatten_params, rngmix, unflatten_params)
+
+# See https://github.com/google/jax/issues/9454.
+tf.config.set_visible_devices([], "GPU")
+
+def plot_interp_loss(epoch, lambdas, train_loss_interp_naive, test_loss_interp_naive,
+                     train_loss_interp_clever, test_loss_interp_clever):
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.plot(lambdas,
+          train_loss_interp_naive,
+          linestyle="dashed",
+          color="tab:blue",
+          alpha=0.5,
+          linewidth=2,
+          label="Train, naïve interp.")
+  ax.plot(lambdas,
+          test_loss_interp_naive,
+          linestyle="dashed",
+          color="tab:orange",
+          alpha=0.5,
+          linewidth=2,
+          label="Test, naïve interp.")
+  ax.plot(lambdas,
+          train_loss_interp_clever,
+          linestyle="solid",
+          color="tab:blue",
+          linewidth=2,
+          label="Train, permuted interp.")
+  ax.plot(lambdas,
+          test_loss_interp_clever,
+          linestyle="solid",
+          color="tab:orange",
+          linewidth=2,
+          label="Test, permuted interp.")
+  ax.set_xlabel("$\lambda$")
+  ax.set_xticks([0, 1])
+  ax.set_xticklabels(["Model $A$", "Model $B$"])
+  ax.set_ylabel("Loss")
+  # TODO label x=0 tick as \theta_1, and x=1 tick as \theta_2
+  ax.set_title(f"Loss landscape between the two models (epoch {epoch})")
+  ax.legend(loc="upper right", framealpha=0.5)
+  fig.tight_layout()
+  return fig
+
+def plot_interp_acc(epoch, lambdas, train_acc_interp_naive, test_acc_interp_naive,
+                    train_acc_interp_clever, test_acc_interp_clever):
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.plot(lambdas,
+          train_acc_interp_naive,
+          linestyle="dashed",
+          color="tab:blue",
+          alpha=0.5,
+          linewidth=2,
+          label="Train, naïve interp.")
+  ax.plot(lambdas,
+          test_acc_interp_naive,
+          linestyle="dashed",
+          color="tab:orange",
+          alpha=0.5,
+          linewidth=2,
+          label="Test, naïve interp.")
+  ax.plot(lambdas,
+          train_acc_interp_clever,
+          linestyle="solid",
+          color="tab:blue",
+          linewidth=2,
+          label="Train, permuted interp.")
+  ax.plot(lambdas,
+          test_acc_interp_clever,
+          linestyle="solid",
+          color="tab:orange",
+          linewidth=2,
+          label="Test, permuted interp.")
+  ax.set_xlabel("$\lambda$")
+  ax.set_xticks([0, 1])
+  ax.set_xticklabels(["Model $A$", "Model $B$"])
+  ax.set_ylabel("Accuracy")
+  # TODO label x=0 tick as \theta_1, and x=1 tick as \theta_2
+  ax.set_title(f"Accuracy between the two models (epoch {epoch})")
+  ax.legend(loc="lower right", framealpha=0.5)
+  fig.tight_layout()
+  return fig
+
+class PermutationSpec(NamedTuple):
+  perm_to_axes: dict
+  axes_to_perm: dict
+
+def permutation_spec_from_axes_to_perm(axes_to_perm: dict) -> PermutationSpec:
+  perm_to_axes = defaultdict(list)
+  for wk, axis_perms in axes_to_perm.items():
+    for axis, perm in enumerate(axis_perms):
+      if perm is not None:
+        perm_to_axes[perm].append((wk, axis))
+  return PermutationSpec(perm_to_axes=dict(perm_to_axes), axes_to_perm=axes_to_perm)
+
+def mlp_permutation_spec(num_hidden_layers: int) -> PermutationSpec:
+  """We assume that one permutation cannot appear in two axes of the same weight array."""
+  assert num_hidden_layers >= 1
+  return permutation_spec_from_axes_to_perm({
+      "Dense_0/kernel": (None, "P_0"),
+      **{f"Dense_{i}/kernel": (f"P_{i-1}", f"P_{i}")
+         for i in range(1, num_hidden_layers)},
+      **{f"Dense_{i}/bias": (f"P_{i}", )
+         for i in range(num_hidden_layers)},
+      f"Dense_{num_hidden_layers}/kernel": (f"P_{num_hidden_layers-1}", None),
+      f"Dense_{num_hidden_layers}/bias": (None, ),
+  })
+
+def vgg16_permutation_spec() -> PermutationSpec:
+  return permutation_spec_from_axes_to_perm({
+      "Conv_0/kernel": (None, None, None, "P_Conv_0"),
+      **{f"Conv_{i}/kernel": (None, None, f"P_Conv_{i-1}", f"P_Conv_{i}")
+         for i in range(1, 13)},
+      **{f"Conv_{i}/bias": (f"P_Conv_{i}", )
+         for i in range(13)},
+      **{f"LayerNorm_{i}/scale": (f"P_Conv_{i}", )
+         for i in range(13)},
+      **{f"LayerNorm_{i}/bias": (f"P_Conv_{i}", )
+         for i in range(13)},
+      "Dense_0/kernel": ("P_Conv_12", "P_Dense_0"),
+      "Dense_0/bias": ("P_Dense_0", ),
+      "Dense_1/kernel": ("P_Dense_0", "P_Dense_1"),
+      "Dense_1/bias": ("P_Dense_1", ),
+      "Dense_2/kernel": ("P_Dense_1", None),
+      "Dense_2/bias": (None, ),
+  })
+
+def get_permuted_param(ps: PermutationSpec, perm, k: str, params, except_axis=None):
+  """Get parameter `k` from `params`, with the permutations applied."""
+  w = params[k]
+  for axis, p in enumerate(ps.axes_to_perm[k]):
+    # Skip the axis we're trying to permute.
+    if axis == except_axis:
+      continue
+
+    # None indicates that there is no permutation relevant to that axis.
+    if p is not None:
+      w = jnp.take(w, perm[p], axis=axis)
+
+  return w
+
+def apply_permutation(ps: PermutationSpec, perm, params):
+  """Apply a `perm` to `params`."""
+  return {k: get_permuted_param(ps, perm, k, params) for k in params.keys()}
+
+def weight_matching(rng, ps: PermutationSpec, params_a, params_b, max_iter=100, init_perm=None):
+  """Find a permutation of `params_b` to make them match `params_a`."""
+  perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]] for p, axes in ps.perm_to_axes.items()}
+
+  perm = {p: jnp.arange(n) for p, n in perm_sizes.items()} if init_perm is None else init_perm
+  perm_names = list(perm.keys())
+
+  for iteration in range(max_iter):
+    progress = False
+    for p_ix in random.permutation(rngmix(rng, iteration), len(perm_names)):
+      p = perm_names[p_ix]
+      n = perm_sizes[p]
+      A = jnp.zeros((n, n))
+      for wk, axis in ps.perm_to_axes[p]:
+        w_a = params_a[wk]
+        w_b = get_permuted_param(ps, perm, wk, params_b, except_axis=axis)
+        w_a = jnp.moveaxis(w_a, axis, 0).reshape((n, -1))
+        w_b = jnp.moveaxis(w_b, axis, 0).reshape((n, -1))
+        A += w_a @ w_b.T
+
+      ri, ci = linear_sum_assignment(A, maximize=True)
+      assert (ri == jnp.arange(len(ri))).all()
+
+      oldL = jnp.vdot(A, jnp.eye(n)[perm[p]])
+      newL = jnp.vdot(A, jnp.eye(n)[ci, :])
+      print(f"{iteration}/{p}: {newL - oldL}")
+      progress = progress or newL > oldL + 1e-12
+
+      perm[p] = jnp.array(ci)
+
+    if not progress:
+      break
+
+  return perm
+
+# def main():
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--model-a", type=str, required=True)
+  parser.add_argument("--model-b", type=str, required=True)
+  parser.add_argument("--seed", type=int, default=0, help="Random seed")
+  parser.add_argument("--width-multiplier", type=int, required=True)
+  args = parser.parse_args()
+
+  with wandb.init(
+      project="playing-the-lottery",
+      entity="skainswo",
+      tags=["cifar10", "vgg16", "straight-through-estimator-2"],
+      # See https://github.com/wandb/client/issues/3672.
+      mode="online",
+      job_type="analysis",
+  ) as wandb_run:
+    config = wandb.config
+    config.ec2_instance_type = ec2_get_instance_type()
+    config.model_a = args.model_a
+    config.model_b = args.model_b
+    config.seed = args.seed
+    config.width_multiplier = args.width_multiplier
+    config.num_epochs = 1000
+    config.batch_size = 500
+    config.learning_rate = 1e-2
+    # This is the epoch that we pull the model A/B params from.
+    config.load_epoch = 99
+
+    config.params_represent = "midpoint"
+    # config.params_represent = "b_endpoint"
+    # config.projection_type = "l2"
+    config.projection_type = "inner_product"
+
+    model = make_vgg_width_ablation(config.width_multiplier)
+
+    def load_model(filepath):
+      with open(filepath, "rb") as fh:
+        return from_bytes(
+            init_train_state(random.PRNGKey(0),
+                             model,
+                             learning_rate=-1,
+                             num_epochs=100,
+                             batch_size=100,
+                             num_train_examples=50_000), fh.read())
+
+    artifact_a = Path(wandb_run.use_artifact(f"cifar10-vgg-weights:{config.model_a}").download())
+    artifact_b = Path(wandb_run.use_artifact(f"cifar10-vgg-weights:{config.model_b}").download())
+    model_a = load_model(artifact_a / f"checkpoint{config.load_epoch}")
+    model_b = load_model(artifact_b / f"checkpoint{config.load_epoch}")
+
+    stuff = make_stuff(model)
+    train_ds, test_ds = load_datasets()
+    num_train_examples = train_ds["images_u8"].shape[0]
+    num_test_examples = test_ds["images_u8"].shape[0]
+    assert num_train_examples % config.batch_size == 0
+
+    assert num_test_examples % config.batch_size == 0
+
+    train_loss_a, train_accuracy_a = stuff["dataset_loss_and_accuracy"](model_a.params, train_ds,
+                                                                        10_000)
+    train_loss_b, train_accuracy_b = stuff["dataset_loss_and_accuracy"](model_b.params, train_ds,
+                                                                        10_000)
+    test_loss_a, test_accuracy_a = stuff["dataset_loss_and_accuracy"](model_a.params, test_ds,
+                                                                      10_000)
+    test_loss_b, test_accuracy_b = stuff["dataset_loss_and_accuracy"](model_b.params, test_ds,
+                                                                      10_000)
+
+    print({
+        "train_loss_a": train_loss_a,
+        "train_accuracy_a": train_accuracy_a,
+        "train_loss_b": train_loss_b,
+        "train_accuracy_b": train_accuracy_b,
+        "test_loss_a": test_loss_a,
+        "test_accuracy_a": test_accuracy_a,
+        "test_loss_b": test_loss_b,
+        "test_accuracy_b": test_accuracy_b,
+    })
+
+    baseline_train_loss = 0.5 * (train_loss_a + train_loss_b)
+
+    @jit
+    def batch_eval(params, projected_params, model_a_params, images_u8, labels):
+      # See https://jax.readthedocs.io/en/latest/jax-101/04-advanced-autodiff.html#straight-through-estimator-using-stop-gradient
+      ste_params = tree_map(lambda x, px: stop_gradient(px) + (x - stop_gradient(x)), params,
+                            projected_params)
+
+      # "params <~> midpoint" else, "params <~> B endpoint"
+      eval_params = ste_params if config.params_represent == "midpoint" else tree_map(
+          lambda x, y: 0.5 * (x + y), ste_params, freeze({"params": model_a_params}))
+
+      l, info = stuff["batch_eval"](eval_params["params"], images_u8, labels)
+
+      # Makes life easier to know when we're winning. stop_gradient shouldn't be
+      # necessary but I'm paranoid.
+      l -= stop_gradient(baseline_train_loss)
+
+      return l, {**info, "accuracy": info["num_correct"] / config.batch_size}
+
+    @jit
+    def step(train_state, projected_params, images_u8, labels):
+      (l, metrics), g = value_and_grad(batch_eval, has_aux=True)(
+          train_state.params,
+          projected_params,
+          model_a.params,
+          images_u8,
+          labels,
+      )
+      train_state = train_state.apply_gradients(grads=g)
+      return train_state, {**metrics, "loss": l}
+
+    rng = random.PRNGKey(config.seed)
+
+    tx = optax.sgd(learning_rate=config.learning_rate, momentum=0.9)
+    # tx = optax.radam(learning_rate=config.learning_rate)
+
+    permutation_spec = vgg16_permutation_spec()
+
+    # init_params = model.init(rngmix(rng, "init"), jnp.zeros((1, 32, 32, 3)))["params"]
+
+    # Better init when projecting with L2?
+    # init_perm = weight_matching(rngmix(rng, "weight_matching"), permutation_spec,
+    #                             flatten_params(model_a.params), flatten_params(model_b.params))
+    # init_params = unflatten_params(
+    #     apply_permutation(permutation_spec, init_perm, flatten_params(model_b.params)))
+
+    # init_params = tree_map(lambda x, y: 0.5 * (x + y), init_params, model_a.params)
+
+    init_params = model_a.params
+
+    # init_params = tree_map(lambda x, y: 0.5 * (x + y), model_a.params, model_b.params)
+
+    train_state = TrainState.create(apply_fn=None, params=freeze({"params": init_params}), tx=tx)
+
+    perm = None
+
+    for epoch in tqdm(range(config.num_epochs)):
+      train_data_perm = random.permutation(rngmix(rng, f"epoch-{epoch}"),
+                                           num_train_examples).reshape((-1, config.batch_size))
+      for i in range(num_train_examples // config.batch_size):
+        if config.projection_type == "inner_product":
+          perm = weight_matching(rngmix(rng, f"epoch-{epoch}-{i}"),
+                                 permutation_spec,
+                                 flatten_params(train_state.params["params"]),
+                                 flatten_params(model_b.params),
+                                 max_iter=100,
+                                 init_perm=perm)
+        if config.projection_type == "l2":
+          perm = weight_matching(
+              rngmix(rng, f"epoch-{epoch}-{i}"),
+              permutation_spec,
+              flatten_params(
+                  tree_map(lambda w, w_a: 2 * w - w_a, train_state.params["params"],
+                           model_a.params)),
+              flatten_params(model_b.params),
+              max_iter=100,
+              init_perm=perm,
+          )
+
+        # "params <~> midpoint", else "params <~> B endpoint"
+        if config.params_represent == "midpoint":
+          projected_params = tree_map(
+              lambda x, y: 0.5 * (x + y), model_a.params,
+              unflatten_params(
+                  apply_permutation(permutation_spec, perm, flatten_params(model_b.params))))
+        if config.params_represent == "b_endpoint":
+          projected_params = unflatten_params(
+              apply_permutation(permutation_spec, perm, flatten_params(model_b.params)))
+
+        # Good ole' SGD, no STE
+        # projected_params = train_state.params["params"]
+
+        projection_l2 = jnp.sqrt(
+            tree_reduce(
+                lambda x, y: x + y,
+                tree_map(lambda x, y: jnp.sum(jnp.square(x - y)),
+                         flatten_params(train_state.params["params"]),
+                         flatten_params(projected_params))))
+
+        train_state, metrics = step(train_state, freeze({"params": projected_params}),
+                                    train_ds["images_u8"][train_data_perm[i]],
+                                    train_ds["labels"][train_data_perm[i]])
+        wandb_run.log({**metrics, "projection_l2": projection_l2})
+
+        if not jnp.isfinite(metrics["loss"]):
+          raise ValueError(f"Loss is not finite: {metrics['loss']}")
+
+    raise
+    final_permutation = {k: jnp.argsort(lsa(v)) for k, v in train_state.params.items()}
+
+    # Save final_permutation as an Artifact
+    artifact = wandb.Artifact("model_b_permutation",
+                              type="permutation",
+                              metadata={
+                                  "dataset": "mnist",
+                                  "model": "mlp"
+                              })
+    with artifact.new_file("permutation.pkl", mode="wb") as f:
+      pickle.dump(final_permutation, f)
+    wandb_run.log_artifact(artifact)
+
+    ### plotting
+    lambdas = jnp.linspace(0, 1, num=25)
+    train_loss_interp_naive = []
+    test_loss_interp_naive = []
+    train_acc_interp_naive = []
+    test_acc_interp_naive = []
+    for lam in tqdm(lambdas):
+      naive_p = tree_map(lambda a, b: (1 - lam) * a + lam * b, model_a.params, model_b.params)
+      train_loss, train_acc = stuff["dataset_loss_and_accuracy"](naive_p, train_ds, 10_000)
+      test_loss, test_acc = stuff["dataset_loss_and_accuracy"](naive_p, test_ds, 10_000)
+      train_loss_interp_naive.append(train_loss)
+      test_loss_interp_naive.append(test_loss)
+      train_acc_interp_naive.append(train_acc)
+      test_acc_interp_naive.append(test_acc)
+
+    model_b_clever = apply_permutation(final_permutation, model_b.params)
+
+    train_loss_interp_clever = []
+    test_loss_interp_clever = []
+    train_acc_interp_clever = []
+    test_acc_interp_clever = []
+    for lam in tqdm(lambdas):
+      clever_p = tree_map(lambda a, b: (1 - lam) * a + lam * b, model_a.params, model_b_clever)
+      train_loss, train_acc = stuff["dataset_loss_and_accuracy"](clever_p, train_ds, 10_000)
+      test_loss, test_acc = stuff["dataset_loss_and_accuracy"](clever_p, test_ds, 10_000)
+      train_loss_interp_clever.append(train_loss)
+      test_loss_interp_clever.append(test_loss)
+      train_acc_interp_clever.append(train_acc)
+      test_acc_interp_clever.append(test_acc)
+
+    assert len(lambdas) == len(train_loss_interp_naive)
+    assert len(lambdas) == len(test_loss_interp_naive)
+    assert len(lambdas) == len(train_acc_interp_naive)
+    assert len(lambdas) == len(test_acc_interp_naive)
+    assert len(lambdas) == len(train_loss_interp_clever)
+    assert len(lambdas) == len(test_loss_interp_clever)
+    assert len(lambdas) == len(train_acc_interp_clever)
+    assert len(lambdas) == len(test_acc_interp_clever)
+
+    print("Plotting...")
+    fig = plot_interp_loss(config.load_epoch, lambdas, train_loss_interp_naive,
+                           test_loss_interp_naive, train_loss_interp_clever,
+                           test_loss_interp_clever)
+    plt.savefig(f"mnist_mlp_ste_interp_loss_epoch{config.load_epoch}.png", dpi=300)
+    wandb_run.log({"interp_loss_fig": wandb.Image(fig)}, commit=False)
+    plt.close(fig)
+
+    fig = plot_interp_acc(config.load_epoch, lambdas, train_acc_interp_naive, test_acc_interp_naive,
+                          train_acc_interp_clever, test_acc_interp_clever)
+    plt.savefig(f"mnist_mlp_ste_interp_accuracy_epoch{config.load_epoch}.png", dpi=300)
+    wandb_run.log({"interp_acc_fig": wandb.Image(fig)}, commit=False)
+    plt.close(fig)
+
+    wandb_run.log({}, commit=True)
+
+# if __name__ == "__main__":
+#   main()
